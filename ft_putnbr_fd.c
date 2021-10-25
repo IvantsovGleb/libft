d57733b39fd9463f5f88d9ff5d7a94d7cc07b763
Gleb Ivantsov <ivantsovgleb@icloud.com>
2021-10-25 11:18:45 +0300

    fixed

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -2,9 +2,15 @@
 
 void	ft_putnbr_fd(int n, int fd)
 {
-	char	*nstr;
+	long	num;
 
-	nstr = ft_itoa(n);
-	if (fd > 0 && nstr)
-		ft_putstr_fd(nstr, fd);
+	num = n;
+	if (n < 0)
+	{
+		ft_putchar_fd('-', fd);
+		num *= -1;
+	}
+	if (num >= 10)
+		ft_putnbr_fd(num / 10, fd);
+	ft_putchar_fd((num % 10) + '0', fd);
 }
