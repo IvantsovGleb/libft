e020b420e90704e2f135b28106c4162983cc1319
Gleb Ivantsov <ivantsovgleb@icloud.com>
2021-10-25 10:07:27 +0300

    fixed ft_strtrim.c

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -1,19 +1,24 @@
 #include "libft.h"
 
-char	*ft_strtrim(char const *s1, char const *set)
+char    *ft_strtrim(char const *s1, char const *set)
 {
-	const char	*p;
-	size_t		len;
+        size_t          len;
+        size_t          start;
+        char            *ts;
 
-	p = s1;
-	if (!s1)
-		return (NULL);
-	while (*p && ft_strchr(set, *p))
-		p++;
-	len = ft_strlen(s1);
-	while (len && ft_strchr(set, p[len]))
-		len--;
+        if (!s1)
+                return (NULL);
+        start = 0;
+        while (s1[start] && ft_strchr(set, s1[start]))
+                start++;
+        len = ft_strlen(s1) - 1;
+        while (len && ft_strchr(set, s1[len]))
+                len--;
 	if (len == 0)
-		return (ft_substr(--p, 0, 0));
-	return (ft_substr(p, 0, len + 1));
+		return (ft_calloc(1, 1));
+        ts = malloc(len - start + 2);
+        if (!ts)
+                return (NULL);
+        ft_strlcpy(ts, &s1[start], len - start + 2);
+        return (ts);
 }
