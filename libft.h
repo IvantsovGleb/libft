e6b85a30b66dd3ab949d5c6b067baa354524db47
Gleb Ivantsov <glebivantsov@192.168.1.7>
2021-12-03 02:00:43 +0300

    final mandatory part

--- libft.h
+++ libft.h
@@ -15,12 +15,6 @@
 # include <stdlib.h>
 # include <unistd.h>
 
-typedef struct s_list
-{
-    void            *content;
-    struct s_list   *next;
-}                   t_list;
-
 int		ft_tolower(int c);
 int		ft_toupper(int c);
 int		ft_isalnum(int c);
@@ -55,5 +49,4 @@ void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
 void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
-t_list	*ft_lstnew(void *content);
 #endif
